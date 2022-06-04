445e13237c55ba7ef100493661452f68ea11e48c
sphixy <oumarbensalem99@gmail.com>
2022-06-04 14:36:00 +0200

    test commit
    commit pour test

---
+++ index.js
@@ -0,0 +1,31 @@
+//moblie menu
+const BurgerIcon = document.querySelector("#burger")
+const navbarMenu = document.querySelector("#navbar-links")
+ 
+BurgerIcon.addEventListener("click" , () => {
+    navbarMenu.classList.toggle("is-active")
+    console.log("e")
+})
+
+
+
+// tabs
+//const tabs = document.querySelectorAll('.tabs li');
+//const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+//tabs.forEach(tab => {
+ // tab.addEventListener('click', () => {
+ //   tabs.forEach(item => item.classList.remove('is-active'));
+   // tab.classList.add('is-active');
+
+    //const target = tab.dataset.target;
+    // console.log(target);
+    //tabContentBoxes.forEach(box => {
+      //if (box.getAttribute('id') === target) {
+        //box.classList.remove('is-hidden');
+      //} else {
+       // box.classList.add('is-hidden');
+      //}
+    //})
+  //})
+//})
